de133ccc05e3ccc05c194d46e7f0f631dc60917b
Steph G <61924023+sbgastondesign@users.noreply.github.com>
2021-08-09 13:54:54 -0400

    server updated

--- server.js
+++ server.js
@@ -1,15 +1,14 @@
 // Dependencies
 const express = require("express");
 const path = require("path");
-const fs = require('fs');
-const note = require('./api/notes.js');
-const getNotes = require('./db/db.json');
+// const db = require('./publc/assets/js/index');
+// const fs = require('fs');
 
-// notes = [];
+const notes = [];
 
 // Sets up the Express App
 const app = express();
-const PORT = process.env.Port || 8080;
+const PORT = process.env.Port || 3001;
 
 // Sets up the Express app to handle data parsing
 app.use(express.urlencoded({ extended: true }));
@@ -19,28 +18,32 @@ app.use(express.static("public"));
 
 // DISPLAY ROUTES
 // Basic route that sends the user first to the AJAX Page
-app.get("/", (req, res) => res.sendFile(path.join(__dirname, "../public/index.html")));
+app.get("/", (req, res) => res.sendFile(path.join(__dirname, "./public/index.html")));
 app.get("/notes", (req, res) =>
-    res.sendFile(path.join(__dirname, "../public/notes.html"))
+    res.sendFile(path.join(__dirname, "./public/notes.html"))
 );
 
 //API Route
-app.get("/api/notes", (req, res) => res.json(note));
-// res.json(notes));
+app.get('/api/notes', (req, res) =>
+    res.json(notes));
 
+// res.json(notes));
+app.post('/api/notes', (req, res) => {
+    const addNote = req.body;
+    res.json(addNote);
+}
 
 // Posts Notes
-app.post("./api/notes", (req, res) => {
+// app.post("./api/notes", (req, res) => {
 // const newNote = req.body
 // res.JSON(newNote)
 
-    let notes = JSON.parse(fs.readFileSync(path.join(__dirname, "./public/db/db.json"), "utf8"))
-    getNotes.push(req.body.note)
-    fs.writeFileSync(path.join(__dirname, "../public/db/db.json"), JSON.stringify(notes))
-    console.log("note submitted")
-    res.send("Note success!")
-    
-})
+//     let notes = JSON.parse(fs.readFileSync(path.join(__dirname, "./public/db/db.json"), "utf8"))
+//     getNotes.push(req.body.note)
+//     fs.writeFileSync(path.join(__dirname, "../public/db/db.json"), JSON.stringify(notes))
+//     console.log("note submitted")
+//     res.send("Note success!")
+);
 
 
 // Starts the server to begin listening
