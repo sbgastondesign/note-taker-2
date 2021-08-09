e18ab0b89323bb3f51601375f030568febaf66a7
Steph G <61924023+sbgastondesign@users.noreply.github.com>
2021-08-09 14:14:49 -0400

    server updated

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const notes = [];
 
 // Sets up the Express App
 const app = express();
-const PORT = process.env.Port || 3001;
+const PORT = process.env.Port || 80;
 
 // Sets up the Express app to handle data parsing
 app.use(express.urlencoded({ extended: true }));
@@ -18,9 +18,9 @@ app.use(express.static("public"));
 
 // DISPLAY ROUTES
 // Basic route that sends the user first to the AJAX Page
-app.get("/", (req, res) => res.sendFile(path.join(__dirname, "./public/index.html")));
+app.get("/", (req, res) => res.sendFile(path.join(__dirname, "public/index.html")));
 app.get("/notes", (req, res) =>
-    res.sendFile(path.join(__dirname, "./public/notes.html"))
+    res.sendFile(path.join(__dirname, "public/notes.html"))
 );
 
 //API Route
